fef7eb1170e8778eae01fdb501d063c864eb1ee4
hongwang <songhw@tangdou.com>
2018-03-30 23:57:21 +0800

    首页农气产品

--- app/src/main/java/com/cbt/main/fragment/IndexFragment.java
+++ app/src/main/java/com/cbt/main/fragment/IndexFragment.java
@@ -6,16 +6,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.cbt.main.R;
+import com.cbt.main.adapter.IndexProductAdapter;
 import com.cbt.main.engin.SceneSurfaceView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by vigorous on 16/1/4.
  * 首页
  */
 public class IndexFragment extends BaseFragment {
+
+    @Nullable@BindView(R.id.listView)
+    ListView mLv;
+    private IndexProductAdapter mIndexProductAdapter;
 
     @Nullable
     @Override
@@ -28,7 +40,24 @@ public class IndexFragment extends BaseFragment {
 
     @Override
     public void initUI() {
+        mLv = (ListView) mRootView.findViewById(R.id.listView) ;
+        mRootView.findViewById(R.id.tv_product_more).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
         mTvTitle.setText("北京市海淀区");
+        List<String> dataList = new ArrayList<>();
+        dataList.add("aaa");
+        dataList.add("aaa");
+        dataList.add("aaa");
+        dataList.add("aaa");
+        dataList.add("aaa");
+        mIndexProductAdapter = new IndexProductAdapter(dataList, getActivity());
+        mLv.setAdapter(mIndexProductAdapter);
     }
 
     @Override
