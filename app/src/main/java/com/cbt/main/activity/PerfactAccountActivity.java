691791557a1eb783d23560cd82e2d89601433544
hongwang <songhw@tangdou.com>
2018-04-01 16:58:23 +0800

    完善个人信息

---
+++ app/src/main/java/com/cbt/main/activity/PerfactAccountActivity.java
@@ -0,0 +1,21 @@
+package com.cbt.main.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by vigorous on 18/4/1.
+ * 完善个人信息
+ */
+
+public class PerfactAccountActivity extends BaseActivity{
+    @Override
+    public void onCCreate(@Nullable Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void initUI() {
+
+    }
+}
