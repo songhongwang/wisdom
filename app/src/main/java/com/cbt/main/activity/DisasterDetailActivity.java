d2a807c876c96d9c6e8d0b2672ccc0c2c7dc7d0c
hongwang <songhw@tangdou.com>
2018-04-10 21:54:33 +0800

    详情页面

--- app/src/main/java/com/cbt/main/activity/DisasterDetailActivity.java
+++ app/src/main/java/com/cbt/main/activity/DisasterDetailActivity.java
@@ -2,14 +2,28 @@ package com.cbt.main.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.cbt.main.R;
+import com.cbt.main.adapter.DisasterActAdapter;
+import com.cbt.main.adapter.ReleaseActAdapter;
+import com.cbt.main.view.piaoquan.MessagePicturesLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by vigorous on 18/4/10.
  */
 
 public class DisasterDetailActivity extends BaseActivity {
+    private DisasterActAdapter mDisasterActAdapter;
+
+    MessagePicturesLayout lPictures;
+    TextView mTvContent;
+    ListView mListView;
     @Override
     public void onCCreate(@Nullable Bundle savedInstanceState) {
         setContentView(R.layout.activity_disaster);
@@ -18,5 +32,31 @@ public class DisasterDetailActivity extends BaseActivity {
     @Override
     public void initUI() {
 
+        mIvFinish.setVisibility(View.GONE);
+
+        mListView = (ListView) findViewById(R.id.lv_disaster);
+        View headerView = View.inflate(this, R.layout.header_disaster_list, null);
+        mListView.addHeaderView(headerView);
+
+        List datas  = new ArrayList();
+
+        mDisasterActAdapter = new DisasterActAdapter(this,datas);
+        mListView.setAdapter(mDisasterActAdapter);
+
+
+        lPictures = (MessagePicturesLayout) headerView.findViewById(R.id.l_pictures);
+        mTvContent = (TextView) headerView.findViewById(R.id.t_content);
+
+
+
+        datas.add("https://avatar.csdn.net/D/A/2/3_bigdog_1027.jpg");
+        datas.add("https://avatar.csdn.net/D/A/2/3_bigdog_1027.jpg");
+        datas.add("https://avatar.csdn.net/D/A/2/3_bigdog_1027.jpg");
+        datas.add("https://avatar.csdn.net/D/A/2/3_bigdog_1027.jpg");
+        datas.add("https://avatar.csdn.net/D/A/2/3_bigdog_1027.jpg");
+        datas.add("https://avatar.csdn.net/D/A/2/3_bigdog_1027.jpg");
+        datas.add("https://avatar.csdn.net/D/A/2/3_bigdog_1027.jpg");
+
+        lPictures.set(datas, datas);
     }
 }
---
+++ app/src/main/java/com/cbt/main/adapter/DisasterActAdapter.java
@@ -0,0 +1,34 @@
+package com.cbt.main.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.cbt.main.R;
+
+import java.util.List;
+
+/**
+ * Created by vigorous on 17/12/27.
+ */
+
+public class DisasterActAdapter extends AppBaseAdapter {
+    public DisasterActAdapter(Context context, List<String> dataList) {
+        super(dataList, context);
+    }
+
+    @Override
+    public View getCView(int position, View view, ViewGroup viewGroup) {
+        View itemView = mInflater.inflate(R.layout.item_disaster_activity, null, false);
+        ImageView iv = (ImageView) itemView.findViewById(R.id.iv_img);
+
+        String s = (String) mDataList.get(position);
+        if(!s.equals("holder")){
+            Glide.with(mContext).load(s).into(iv);
+        }
+
+        return itemView;
+    }
+}
