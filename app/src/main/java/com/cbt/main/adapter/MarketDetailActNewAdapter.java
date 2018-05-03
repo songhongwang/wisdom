318eb588bf86c283ecec0609dcab8a86e11d1030
hongwang <songhw@tangdou.com>
2018-05-03 14:28:59 +0800

    市场详情用户头像

--- app/src/main/java/com/cbt/main/adapter/MarketDetailActNewAdapter.java
+++ app/src/main/java/com/cbt/main/adapter/MarketDetailActNewAdapter.java
@@ -1,6 +1,8 @@
 package com.cbt.main.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,8 +10,12 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.cbt.main.R;
+import com.cbt.main.activity.UserActivity;
+import com.cbt.main.model.Data;
 import com.cbt.main.model.ReplayMarketView;
 import com.cbt.main.model.ReplayMyprolemView;
+import com.cbt.main.utils.net.Constants;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -28,7 +34,7 @@ public class MarketDetailActNewAdapter extends AppBaseAdapter {
 
     @Override
     public View getCView(int position, View view, ViewGroup viewGroup) {
-        ReplayMarketView mData = (ReplayMarketView)mDataList.get(position);
+        final ReplayMarketView mData = (ReplayMarketView)mDataList.get(position);
         View itemView = mInflater.inflate(R.layout.item_market_detail_activity, null, false);
 //        ImageView iv = (ImageView) itemView.findViewById(R.id.iv_img);
 //
@@ -37,7 +43,27 @@ public class MarketDetailActNewAdapter extends AppBaseAdapter {
 //            Glide.with(mContext).load(s).into(iv);
 //        }
         ImageView iv_img = (ImageView) itemView.findViewById(R.id.iv_img);
-        Glide.with(mContext).load(mData.getReplyicon()).into(iv_img);
+        if(!TextUtils.isEmpty(mData.getReplyicon())){
+            Picasso.with(itemView.getContext()).load(Constants.getBaseUrl() + mData.getReplyicon()).placeholder(R.drawable.login_default_icon)
+                    .transform(mCropCircleTransformation)
+                    .into(iv_img);
+        }else{
+            iv_img.setImageResource(R.drawable.login_default_icon);
+        }
+        iv_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, UserActivity.class);
+                Data data = new Data();
+                data.setUid(mData.getReplayuserid());
+                data.setIid(mData.getReplayuserid());
+                data.setAvatar(mData.getReplyicon());
+                data.setNickname(mData.getReplayusername());
+
+                intent.putExtra("model", data);
+                mContext.startActivity(intent);
+            }
+        });
 
         TextView tv_name = (TextView) itemView.findViewById(R.id.tv_name);
         tv_name.setText(mData.getReplayusername());
