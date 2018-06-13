12f06245c07495b7c2885ce53e3ba165deff4a87
cxp <47083420@qq.com>
2018-06-13 10:33:50 +0800

    Merge remote-tracking branch 'dev2/dev2' into dev2
    # Conflicts:
    #	app/src/main/java/com/cbt/main/utils/net/Constants.java

--- app/src/main/java/com/cbt/main/utils/net/Constants.java
+++ app/src/main/java/com/cbt/main/utils/net/Constants.java
@@ -5,7 +5,7 @@ package com.cbt.main.utils.net;
  */
 
 public class Constants {
-    private static String sBaseUrl = "http://114.215.25.185:2522/hnzhnq/\";";
+    private static String sBaseUrl = "http://114.215.25.185:2522/hnzhnq/";
 //    private static String sBaseUrl = "http://192.168.31.152:9999/hnzhnq/
     private static String sRongBaseUrl = "http://api.cn.ronghub.com";
 
